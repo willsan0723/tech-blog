1bca95a33b0b47f3e88bcac838d03c5717c30827
willsan0723 <william.santee@gmail.com>
2020-11-01 16:58:44 -0700

    figured out and tested logout on inactivity functionality.

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -12,3 +12,35 @@ async function logout() {
 }
 
 document.querySelector('#logout').addEventListener('click', logout);
+
+var timeoutID;
+var warningID;
+
+function setup() {
+    this.addEventListener("mousemove", resetTimer, false);
+    this.addEventListener("mousedown", resetTimer, false);
+    this.addEventListener("keypress", resetTimer, false);
+    this.addEventListener("DOMMouseScroll", resetTimer, false);
+    this.addEventListener("mousewheel", resetTimer, false);
+    this.addEventListener("touchmove", resetTimer, false);
+    this.addEventListener("MSPointerMove", resetTimer, false);
+
+    startTimer();
+}
+setup();
+
+function startTimer() {
+    // wait 6 seconds before calling goInactive
+    timeoutID = window.setTimeout(goInactive, 3600000);
+    warningID = setTimeout(function () { alert("You will be logged out in five minutes due to inactivity") }, 3300000)
+}
+
+function resetTimer(e) {
+    window.clearTimeout(timeoutID);
+    window.clearTimeout(warningID);
+    startTimer()
+}
+
+function goInactive() {
+    logout();
+}
